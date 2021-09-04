37d63c5b4d6c1ee58094db1beb4b48435da9e609
1InfinityDoesExist <patelavinash508@gmail.com>
2021-09-04 21:52:07 +0545

    constructor injection

---
+++ SpringCoreProject_2_ConstructorInjection-1/src/DemoBean.java
@@ -0,0 +1,13 @@
+
+public class DemoBean {
+
+	private String message;
+
+	public DemoBean(String message) {
+		this.message = message;
+	}
+
+	public void show() {
+		System.out.println(message);
+	}
+}
